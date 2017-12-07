51403b3191b5e867de2e6c80fe2806477b520b53
Unknown <loiseau.jc@gmail.com>
2017-12-07 15:47:32 +0100

    Setting up

---
+++ Lecture_2/Slides/python/Newton.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+params = {'text.usetex' : True,
+          'font.size' : 8,
+          'font.family' : 'lmodern',
+          'text.latex.unicode' : True}
+plt.rcParams['text.latex.preamble']=[r'\usepackage{lmodern}']
+plt.rcParams.update(params)
+colors = [ 'dimgrey', 'royalblue', 'orange', 'seagreen', 'y' ]
+
+fig_width = 5.33
+
+if __name__ == '__main__':
+    pass
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Nonlinear_Physics
-# Nonlinear_Physics
+
+This repo contains most of the material for the course on *Nonlinear Physics* given at Ecole Nationale d'Arts et Métiers as part of the Master 2 Program.
