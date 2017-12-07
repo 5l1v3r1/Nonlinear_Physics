a6e2729c35829f13893c5bdc9004b67993b64c94
Unknown <loiseau.jc@gmail.com>
2017-12-07 19:10:46 +0100

    Added Newton-Raphson + minor changes

--- Lecture_2/Slides/python/Flow_on_the_line.py
+++ Lecture_2/Slides/python/Flow_on_the_line.py
@@ -46,6 +46,7 @@ def phase_line_plot(x, f):
 
     #--> Bornes inferieure et superieure pour l'axe des x.
     ax.set_xlim(-8, 8)
+    ax.set_ylim(-1.25, 1.25)
     #--> Label de l'axe des x.
     ax.set_xlabel(r'$x$')
     #--> Positionnement de ce label sur la figure.
@@ -134,6 +135,51 @@ if __name__ == '__main__':
     ax = fig.gca()
 
     ax.plot(t, x, color=colors[1])
+
+    # Set y ticks.
+    ax.set_yticks([0., np.pi/2, np.pi])
+    ax.set_yticklabels([r'~', r'$\displaystyle \frac{\pi}{2}$', r'$\pi$'])
+    ax.set_ylim(-0.1, 1.1*np.pi)
+
+
+    #--> Remplace le cadre habituel de la figure par un systeme d'axes
+    #    centre en (0, 0).
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.spines['bottom'].set_position(('data', -0.1))
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['left'].set_position(('data',0))
+
+    #--> Ajout des fleches au bout des axes.
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+
+    # get width and height of axes object to compute
+    # matching arrowhead length and width
+    dps = fig.dpi_scale_trans.inverted()
+    bbox = ax.get_window_extent().transformed(dps)
+    width, height = bbox.width, bbox.height
+
+    # manual arrowhead width and length
+    hw = 1./20.*(ymax-ymin)
+    hl = 1./20.*(xmax-xmin)
+    lw = 1. # axis line width
+    ohg = 0.3 # arrow overhang
+
+    # compute matching arrowhead length and width
+    yhw = hw/(ymax-ymin)*(xmax-xmin)* height/width
+    yhl = hl/(xmax-xmin)*(ymax-ymin)* width/height
+
+    # draw x and y axis
+    ax.arrow(xmin, -0.1, xmax-xmin, 0., fc='k', ec='k', lw = lw,
+             head_width=hw, head_length=0.25*hl, overhang = ohg,
+             length_includes_head= True, clip_on = False)
+
+    ax.arrow(0, ymin, 0., ymax-ymin, fc='k', ec='k', lw = lw,
+             head_width=yhw, head_length=0.25*yhl, overhang = ohg,
+             length_includes_head= True, clip_on = False)
+
     ax.set_xlabel(r'$t$')
     ax.set_ylabel(r'$x(t)$')
 
